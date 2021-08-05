15bb5ff2bfc0118e680ced73bcf8a3cccbf4f967
Zayar Tun <kyawzayartun.dev@gmail.com>
2021-08-05 18:31:41 +0630

    orcas setup

---
+++ server.js
@@ -0,0 +1,39 @@
+import { ApolloServer } from 'apollo-server'
+import { gql } from 'apollo-server'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    sayHi: () => 'Hello, this is Orcas.',
+  },
+}
+
+const port = process.env.PORT || 5000
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Database connected.')
+    return server.listen({ port })
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`)
+  })
+  .catch((error) => console.log(error))
